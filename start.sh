a9dcc7b2df5efa953ffe01ca9ef88fbe33c61b1b
James Gilliland <jgilliland@apqc.org>
2016-11-23 08:36:38 -0600

    Solr for testing

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ export DOCUMENT_ROOT=${DOCUMENT_ROOT:-"/var/www/html"}
 export NGINX_DIR=${NGINX_DIR:-"/etc/nginx"}
 export MYSQL_DATA=${MYSQL_DATA:-"/var/lib/mysql"}
 export MYSQL_SOCKET=${MYSQL_SOCKET:-"/var/run/mysqld"}
+export SOLR_DATA=${SOLR_DATA:-"/opt/solr/data"}
+export SOLR_CONFIG=${SOLR_CONFIG:-"/opt/solr/conf"}
 
 COMPOSE="docker-compose -f docker-compose.yml"
 
